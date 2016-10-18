33f5d4a31d3ac62a9c8fcedfc1ff7207f24dc03d
Jdway <jdw.davidjaw@gmail.com>
2016-10-18 05:25:17 -0400

    add basic route / pug

--- LAB11/server.js
+++ LAB11/server.js
@@ -1,9 +1,31 @@
-/* global require, console */
 var express = require('express');
 var app = express();
 var port = 3000;
+var pug = require('pug');
 
-app.get('/*', express.static(__dirname + '/app'));
+var router = {};
+router.article = require('./route/article');
+router.protfolio = require('./route/protfolio');
+
+
+app.use('/scripts', express.static(__dirname + '/app/scripts'));
+app.use('/styles', express.static(__dirname + '/app/styles'));
+
+app.use(function(req, res, next) {
+  console.log('Visitor: ' + req.connection.remoteAddress);
+  next();
+});
+
+app.use('^/$', function(req, res) {
+  var indexInfo = { viewName: 'index', };
+  var indexContent = pug.renderFile('./app/container.pug', { basedir: './app/template/' }, indexInfo);
+  res.send(indexContent);
+});
+app.use('/index', function(req, res) {
+  var indexInfo = { viewName: 'index', };
+  var indexContent = pug.renderFile('./app/container.pug', { basedir: './app/template/' }, indexInfo);
+  res.send(indexContent);
+});
 
 app.listen(port, function() {
   console.log('Listening on port :' + port);
