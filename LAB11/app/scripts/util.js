479a2ab31ea7e48c1cfb4388034333a16e442bd4
Jdway <jdw.davidjaw@gmail.com>
2016-10-18 23:20:35 -0400

    header, container, footer and jquery, bootstrap CDN.

--- LAB11/app/scripts/util.js
+++ LAB11/app/scripts/util.js
@@ -2,6 +2,6 @@
 window.onload = function() {
   var welcomeElement = document.createElement('div');
   welcomeElement.innerHTML = '<h3>我的JS成功載入了!!!</h3>';
-  document.getElementsByTagName('body')[0].appendChild(welcomeElement);
+  document.querySelector('.container').appendChild(welcomeElement);
 };
 
